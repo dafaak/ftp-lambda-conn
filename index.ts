3f0fc155f53c19e7f96d502279a3960777bb6b9d
Israel <israel.pe@outlook.es>
2023-01-11 12:45:56 -0500

    feat: nuevo paquete ftp, get archivo Satelite.csv

--- index.ts
+++ index.ts
@@ -1,29 +1,48 @@
 import * as ftp from "basic-ftp"
+import * as fs from "fs";
 
+let Client = require('ssh2-sftp-client');
 example().then(
     res => {
-      console.log(res)
+      console.log({res})
     }
 )
     .catch(err => {
       console.error('Vale verga la vida:', err)
     })
 
+// async function example() {
+//   const client = new ftp.Client()
+//   client.ftp.verbose = true
+//   try {
+//     await client.access({
+//       host: "sftp19.sapsf.com",
+//       user: "laborato08-stage",
+//       password: "704M4eNcmd@1@aI",
+//       secure: true
+//     })
+//     console.log(await client.list())
+//     // await client.uploadFrom("README.md", "README_FTP.md")
+//     // await client.downloadTo("README_COPY.md", "README_FTP.md")
+//   } catch (err) {
+//     console.log(err)
+//   }
+//   client.close()
+// }
 async function example() {
-  const client = new ftp.Client()
-  client.ftp.verbose = true
-  try {
-    await client.access({
-      host: "ftp.dlptest.com",
-      user: "dlpuser",
-      password: "rNrKYTX9g7z3RgJRmxWuGHbeu",
-      secure: false
-    })
-    console.log(await client.list())
-    // await client.uploadFrom("README.md", "README_FTP.md")
-    // await client.downloadTo("README_COPY.md", "README_FTP.md")
-  } catch (err) {
-    console.log(err)
-  }
-  client.close()
+  let sftp = new Client();
+  let remotePath = '/LBAGO/Satelite.csv';
+  let destinyFile = fs.createWriteStream('/tmp/Satelite.csv');
+  sftp.connect({
+    // port: '8080',
+    host: "sftp19.sapsf.com",
+    user: "laborato08-stage",
+    password: "704M4eNcmd@1@aI",
+  }).then(() => {
+    return sftp.get(remotePath, destinyFile)
+  }).then(() => {
+    sftp.end();
+  }).catch(err => {
+    console.log(err, 'catch error');
+  });
 }
